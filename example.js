df74f7562941e0d8499daff0eae0ef7e0bf0f4a8
Hugh Kennedy <hughskennedy@gmail.com>
2014-10-05 00:30:59 +0100

    update example, package.json

--- example.js
+++ example.js
@@ -1,23 +1,35 @@
-var canvas = document.createElement('canvas')
-var gl     = require('gl-context')(canvas, render)
-var GIF    = require('./')
-var noop   = (function(){})
+var canvas   = document.createElement('canvas')
+var gl       = require('gl-context')(canvas, render)
+var triangle = require('a-big-triangle')
+var glslify  = require('glslify')
+var GIF      = require('./')
+var noop     = (function(){})
+var pre      = document.querySelector('pre')
 
 var remaining = 60
 var total = remaining
 var gif = GIF(gl, {
     fps: 24
-  , width: 175
-  , height: 175
+  , width: 512
+  , height: 512
 })
+
+var shader = glslify({
+    vert: './example.vert'
+  , frag: './example.frag'
+})(gl)
 
 function render() {
   var t = 1 - (remaining-- / total)
   if (t > 1) return finish()
+  pre.innerHTML = 'Generating GIF... ' + ~~(t * 100) + '%'
   t = (Math.cos(t * Math.PI * 2) + 1) / 2
 
-  gl.clearColor(t, t, t, 1)
-  gl.clear(gl.COLOR_BUFFER_BIT)
+  gl.viewport(0, 0, canvas.width, canvas.height)
+
+  shader.bind()
+  shader.uniforms.t = t
+  triangle(gl)
 
   gif.tick()
 }
